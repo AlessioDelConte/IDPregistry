32633deb5176ff1a9ecec01f80f30676198eb497
Alessio Del Conte <ale.delconte96@gmail.com>
2023-10-09 10:51:30 +0200

    New data

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import pathlib
 from pathlib import Path
 
 from rdflib import ConjunctiveGraph
+from tqdm import tqdm
 
 if __name__ == '__main__':
-    for dir in Path("scraperOutput").iterdir():
+    for dir in Path("scraperOutput2").iterdir():
         idpKG = ConjunctiveGraph()
         if pathlib.Path.is_dir(dir):
-            for file in dir.iterdir():
+            for file in tqdm(dir.iterdir()):
                 if file.suffix == ".nq":
                     idpKG.parse(file, format="nquads")
-            idpKG.serialize(destination=os.path.join("scraped-ttl", f"{dir.stem}.ttl"), format="turtle",
+            idpKG.serialize(destination=os.path.join("scraped-ttl2", f"{dir.stem}.ttl"), format="turtle",
                             base="https://bioschemas.org/crawl/v1/")
